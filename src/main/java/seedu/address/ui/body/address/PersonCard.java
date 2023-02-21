b87b7fcd04ea47e7ec85192f375a62910970f68a
hansstanley <nusstuhss@gmail.com>
2023-02-21 20:43:21 +0800

    Add remark label to UI

--- src/main/java/seedu/address/ui/body/address/PersonCard.java
+++ src/main/java/seedu/address/ui/body/address/PersonCard.java
@@ -37,6 +37,8 @@ public class PersonCard extends UiPart<Region> {
     private Label phone;
     @FXML
     private FlowPane tags;
+    @FXML
+    private Label remark;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
